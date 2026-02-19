3984cba43e7ddc24e1e6c24a3ab3cc3c90ee2028
xxj <xj@wp.pl>
2026-02-19 08:01:56 +0100

    sn01: remove unused code

--- src/SN01/SignalNoiseCompressorGUI.h
+++ src/SN01/SignalNoiseCompressorGUI.h
@@ -20,34 +20,6 @@
 #include "SignalNoiseGR.h"
 
 //------------------------------------------------------------------------------------
-// const
-//------------------------------------------------------------------------------------
-//knob dimensions
-#define SN01_KNOB1_W			80
-#define SN01_KNOB2_W			60
-#define SN01_KNOB1_Y			20
-#define SN01_KNOB2_Y			130
-#define SN01_KNOB1_H			SN01_KNOB1_W + SN01_KNOB1_Y
-#define SN01_KNOB2_H			SN01_KNOB2_W + SN01_KNOB2_Y
-#define SN01_KNOB1_X			135
-#define SN01_KNOB2_X			80
-#define SN01_KNOB_OFFSET		130
-//VU meter size
-#define SN01_NEEDL_X			570
-#define SN01_NEEDL_Y			20
-#define SN01_NEEDL_W			SN01_NEEDL_X + 180
-#define SN01_NEEDL_H			SN01_NEEDL_Y + 80
-//mode switches
-#define SN01_MODES_X			580
-#define SN01_MODES_Y			150
-#define SN01_MODES_SZ			40
-#define SN01_MODES_OFFSET		120
-//nr. of image frames
-#define SN01_KNOB1_FRAMES		97
-#define SN01_KNOB2_FRAMES		97
-#define SN01_NEEDL_FRAMES		100
-
-//------------------------------------------------------------------------------------
 // editor
 //------------------------------------------------------------------------------------
 
@@ -102,10 +74,4 @@ public:
 
 	void paint (juce::Graphics&) override;
 	void resized() override;
-
-//	virtual void setParameter(VstInt32 at, float v);
-//	virtual void valueChanged(CDrawContext* ctx, CControl* ctrl);
-//runtime - custom
-//	void trackMeter(double dB);
-//	void setupMeter(double fs);
 };
